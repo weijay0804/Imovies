93fefaff4a960845bf26b705a5d9b50460ed4a20
Jay <74105438+weijay0804@users.noreply.github.com>
2021-11-23 15:36:54 +0800

    修復 電影排序問題

--- app/main/views.py
+++ app/main/views.py
@@ -4,8 +4,7 @@
 
 '''
 
-from flask import render_template, request, redirect
-from flask.helpers import make_response, url_for
+from flask import render_template, request
 from . import main
 from ..movie_model import Movies
 import random
@@ -21,8 +20,20 @@ def index():
 @main.route('/movies')
 def movies():
     page = request.args.get('page', 1, type=int)
-    
-    pagination = Movies.query.order_by(Movies.vote_average.desc()).paginate(page, per_page = 10, error_out = False)
+    sort_type = request.args.get('sort')
+    desc = request.args.get('desc')
+    if sort_type == 'rate':
+        if desc:
+            pagination = Movies.query.order_by(Movies.vote_average.desc()).paginate(page, per_page = 10, error_out = False)
+        else:
+            pagination = Movies.query.order_by(Movies.vote_average).paginate(page, per_page = 10, error_out = False)
+    elif sort_type == 'year':
+        if desc:
+            pagination = Movies.query.order_by(Movies.release_date.desc()).paginate(page, per_page = 10, error_out = False)
+        else:
+            pagination = Movies.query.order_by(Movies.release_date).paginate(page, per_page = 10, error_out = False)
+    else:
+        pagination = Movies.query.paginate(page, per_page = 10, error_out = False)
     movies = pagination.items
 
-    return render_template('main/movies.html', movies = movies, pagination = pagination)
+    return render_template('main/movies.html', movies = movies, pagination = pagination, sort_type = sort_type, desc = desc)
