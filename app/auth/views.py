b5b5244c306882fbe0c4158c28151f527cc0075d
Jay <74105438+weijay0804@users.noreply.github.com>
2021-11-24 23:13:06 +0800

    重新排列導覽列

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,7 +6,7 @@
 
 
 from flask import render_template, request, flash, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..user_model import Users
 from app import db
@@ -61,3 +61,12 @@ def login():
 
 
     return render_template('auth/login.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    ''' 使用者登出路由 '''
+    
+    logout_user()
+    flash('你已經登出')
+    return redirect(url_for('main.index'))
