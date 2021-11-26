bbd93accc43065cfd2fced25ecf1d13eb982e0df
Jay <74105438+weijay0804@users.noreply.github.com>
2021-11-27 00:25:28 +0800

    修改 使用者個人資料頁面

--- app/user/views.py
+++ app/user/views.py
@@ -11,6 +11,7 @@ from . import user
 from flask import render_template, request, flash, redirect
 from flask_login import login_required
 from ..user_model import Users
+from ..movie_model import Generes
 from app import db
 
 
@@ -30,28 +31,36 @@ def profile(id):
 def edit_profile(id):
     ''' 使用者編輯個人檔案路由 '''
 
+    genres_dict = Generes.generes_en
     user = Users.query.get_or_404(id)
 
     user_datas = {
         'form_name' : user.name or '',
         'form_location' : user.location or '',
         'form_about_me' : user.about_me or '',
+        'form_genres' : set(user.favorite_movie_genres.split(',')) if user.favorite_movie_genres else set()
     }
 
     if request.method == 'POST':
         name = request.form.get('name') 
         location = request.form.get('location') 
         about_me = request.form.get('about_me') 
+        movie_genres = []
+
+        for genre in set(genres_dict.values()):
+            if request.form.get(genre):
+                movie_genres.append(request.form.get(genre))
 
         user.name = name
         user.location = location
         user.about_me = about_me
+        user.favorite_movie_genres = ','.join(movie_genres)
 
         db.session.commit()
         flash('資料已更新')
         return redirect(url_for('user.profile', id = user.id))
 
-    return render_template('user/edit_profile.html', **user_datas)
+    return render_template('user/edit_profile.html', genres_dict = genres_dict, **user_datas)
 
 
 
