a4399ca4d5fc4d892579b3c590100b134054fe59
Jay <74105438+weijay0804@users.noreply.github.com>
2021-12-13 17:17:06 +0800

    test

--- app/main/views.py
+++ app/main/views.py
@@ -167,6 +167,9 @@ def movie(id):
 
     j = json.loads(response.text)
 
+    print('-----------------')
+    print(j)
+
     if j and j.get('results').get('TW'):
         try:
             if j['results'].get('TW').get('flatrate')[0].get('provider_name') == 'Netflix':
